d951dee2a2c16bca68e609d3f4a1cd881d4532c4
DiogoRFerreira <diogo.r.ferreira@ist.utl.pt>
2016-05-28 23:12:17 +0100

    test3
    test3

--- ficheiros de teste/test3.c
+++ ficheiros de teste/test3.c
@@ -18,53 +18,53 @@ int main(){
 		kv_write(kv, i , linha, strlen(linha)+1, 0);
 	}
 	getchar();
-		printf("QUASE1\n");
+		//printf("QUASE1\n");
 	for ( i = 0; i < MAX_VALUES; i++){
 		if(kv_read(kv, i , linha, 1000) == 0){
 			printf ("key - %10u value %s\n", i, linha);
 		}
 	}
 getchar();
-	printf("QUASE2\n");
+	//printf("QUASE2\n");
 	for (i = 0; i < MAX_VALUES; i++){
 		sprintf(linha, "%u", i+2);
 		kv_write(kv, i , linha, strlen(linha)+1, 1);
 	}
 	getchar();
-		printf("QUASE3\n");
+		//printf("QUASE3\n");
 	for ( i = 0; i < MAX_VALUES; i ++){
 		if(kv_read(kv, i , linha, 1000) == 0){
 			printf ("key - %10u value %s\n", i, linha);
 		}
 	}
 	getchar();
-		printf("QUASE4\n");
+		//printf("QUASE4\n");
 	for (i = 0; i < MAX_VALUES; i++){
 		kv_delete(kv, i);
 	}
 	getchar();
-		printf("QUASE5\n");
+		//printf("QUASE5\n");
 	for ( i = 0; i < MAX_VALUES; i ++){
 		if(kv_read(kv, i , linha, 1000) == 0){
 			printf ("key - %10u value %s\n", i, linha);
 		}
 	}
 	getchar();
-		printf("QUASE6\n");
+		//printf("QUASE6\n");
 /************************************************/
 	for (i = MAX_VALUES; i > 0; i--){
 		sprintf(linha, "%u", i);
 		kv_write(kv, i , linha, strlen(linha)+1, 0);
 	}
 	getchar();
-		printf("QUASE7\n");
+		//printf("QUASE7\n");
 	for ( i = 0; i < MAX_VALUES; i ++){
 		if(kv_read(kv, i , linha, 1000) == 0){
 			printf ("key - %10u value %s\n", i, linha);
 		}
 	}
 	getchar();
-		printf("QUASE8\n");
+		//printf("QUASE8\n");
 	for (i = MAX_VALUES; i > 0; i--){
 		sprintf(linha, "%u", i);
 		kv_write(kv, i , linha, strlen(linha)+1, 1);
@@ -76,128 +76,128 @@ getchar();
 		}
 	}
 	getchar();
-		printf("QUASE9\n");
+		//printf("QUASE9\n");
 	for (i = MAX_VALUES; i > 0; i--){
 		kv_delete(kv, i);
 	}
 	getchar();
-		printf("QUASE10\n");
+		//printf("QUASE10\n");
 	for ( i = 0; i < MAX_VALUES; i ++){
 		if(kv_read(kv, i , linha, 1000) == 0){
 			printf ("key - %10u value %s\n", i, linha);
 		}
 	}
 	getchar();
-		printf("QUASE11\n");
+		//printf("QUASE11\n");
 /************************************************/
 	for (i = MAX_VALUES; i > 0; i-=2){
 		sprintf(linha, "%u", i);
 		kv_write(kv, i , linha, strlen(linha)+1, 0);
 	}
 	getchar();
-	printf("QUASE12\n");
+	//printf("QUASE12\n");
 	for ( i = 0; i < MAX_VALUES; i ++){
 		if(kv_read(kv, i , linha, 1000) == 0){
 			printf ("key - %10u value %s\n", i, linha);
 		}
 	}
 	getchar();
-	printf("QUASE13\n");
+	//printf("QUASE13\n");
 	for (i = MAX_VALUES; i > 0; i-=2){
 		sprintf(linha, "%u", i);
 		kv_write(kv, i , linha, strlen(linha)+1, 1);
 	}
 	getchar();
-	printf("QUASE14\n");
+	//printf("QUASE14\n");
 	for ( i = 0; i < MAX_VALUES; i ++){
 		if(kv_read(kv, i , linha, 1000) == 0){
 			printf ("key - %10u value %s\n", i, linha);
 		}
 	}
 	getchar();
-	printf("QUASE15\n");
+	//printf("QUASE15\n");
 	for (i = MAX_VALUES; i > 0; i-=2){
 		kv_delete(kv, i);
 	}
 	getchar();
-	printf("QUASE16\n");
+	//printf("QUASE16\n");
 	for ( i = 0; i < MAX_VALUES; i ++){
 		if(kv_read(kv, i , linha, 1000) == 0){
 			printf ("key - %10u value %s\n", i, linha);
 		}
 	}
 	getchar();
-	printf("QUASE17\n");
+	//printf("QUASE17\n");
 /************************************************/
 	for (i = MAX_VALUES; i > 0; i-=3){
 		sprintf(linha, "%u", i);
 		kv_write(kv, i , linha, strlen(linha)+1, 0);
 	}
 	getchar();
-	printf("QUASE18\n");
+	//printf("QUASE18\n");
 	for ( i = 0; i < MAX_VALUES; i ++){
 		if(kv_read(kv, i , linha, 1000) == 0){
 			printf ("key - %10u value %s\n", i, linha);
 		}
 	}
 	getchar();
-	printf("QUASE19\n");
+	//printf("QUASE19\n");
 	for (i = MAX_VALUES; i > 0; i-=3){
 		sprintf(linha, "%u", i);
 		kv_write(kv, i , linha, strlen(linha)+1, 1);
 	}
 	getchar();
-	printf("QUASE20\n");
+	//printf("QUASE20\n");
 	for ( i = 0; i < MAX_VALUES; i ++){
 		if(kv_read(kv, i , linha, 1000) == 0){
 			printf ("key - %10u value %s\n", i, linha);
 		}
 	}
 	getchar();
-	printf("QUASE21\n");
+	//printf("QUASE21\n");
 	for (i = MAX_VALUES; i > 0; i-=3){
 		kv_delete(kv, i);
 	}
 	getchar();
-	printf("QUASE22\n");
+	//printf("QUASE22\n");
 	for ( i = 0; i < MAX_VALUES; i ++){
 		if(kv_read(kv, i , linha, 1000) == 0){
 			printf ("key - %10u value %s\n", i, linha);
 		}
 	}
 	getchar();
-	printf("QUASE23\n");
+	//printf("QUASE23\n");
 /************************************************/
 	for (i = 0; i < MAX_VALUES; i+=4){
 		sprintf(linha, "%u", i);
 		kv_write(kv, i , linha, strlen(linha)+1, 0);
 	}
 	getchar();
-	printf("QUASE24\n");
+	//printf("QUASE24\n");
 	for ( i = 0; i < MAX_VALUES; i ++){
 		if(kv_read(kv, i , linha, 1000) == 0){
 			printf ("key - %10u value %s\n", i, linha);
 		}
 	}
 	getchar();
-	printf("QUASE25\n");
+	//printf("QUASE25\n");
 	for (i = 0; i < MAX_VALUES; i+=4){
 		sprintf(linha, "%u", i+2);
 		kv_write(kv, i , linha, strlen(linha)+1, 1);
 	}
-	printf("QUASE\n");
+	//printf("QUASE\n");
 getchar();
 	for ( i = 0; i < MAX_VALUES; i ++){
 		if(kv_read(kv, i , linha, 1000) == 0){
 			printf ("key - %10u value %s\n", i, linha);
 		}
 	}	
-	printf("Aqui\n");
+	//printf("Aqui\n");
 	getchar();
 	for (i = 0; i < MAX_VALUES; i+=4){
 		kv_delete(kv, i);
 	}
-	printf("LOL\n");
+	
 	getchar();
 	for ( i = 0; i < MAX_VALUES; i ++){
 		if(kv_read(kv, i , linha, 1000) == 0){
